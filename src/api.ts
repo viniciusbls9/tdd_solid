88c74fe1c07f453555236753002cee7178f9bb62
Vinicius <beneditoloura@gmail.com>
2024-03-14 07:32:57 -0300

    refactor: adjust make_reservation endpoint and create get reservation

--- src/api.ts
+++ src/api.ts
@@ -1,13 +1,27 @@
 import express from "express";
+import { cancelReservation, makeReservation, getReservation } from "./business";
 const app = express();
 app.use(express.json());
 
 app.post("/make_reservation", async function (req, res) {
   const input = req.body;
-  res.json();
+
+  try {
+    const output = await makeReservation(input);
+    res.json(output);
+  } catch (e: any) {
+    res.status(422).json({
+      message: e.message,
+    });
+  }
+});
+
+app.post("/reservations/:reservationId", async function (req, res) {
+  const output = await getReservation(req.params.reservationId);
 });
 
 app.post("/cancel_reservation", async function (req, res) {
+  await cancelReservation(req.body.reservationId);
   res.end();
 });
 
